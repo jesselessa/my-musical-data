50a3acbb1bb491e0b742261719b9fe4f04f17f9f
jesselessa <jessica.elessa@gmail.com>
2025-09-09 06:21:12 +0200

    created a react app

--- api/server.js
+++ api/server.js
@@ -10,31 +10,21 @@ const PORT = process.env.PORT || 3006;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-//* Middlewares
+// Middlewares
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
   })
 );
 
-// Test route
-app.get("/test", (req, res) => {
-  res
-    .json({
-      message: "test",
-      status: "success",
-    })
-    .status(200);
-});
-
-//* Serve static files in production
+// Serve static files in production
 // app.use(express.static(path.join(__dirname, "../client/dist")));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 // });
 
-//* Handle errors globally
+// Handle errors globally
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res
@@ -42,5 +32,5 @@ app.use((err, req, res, next) => {
     .json({ error: err.message || "Internal Server Error" });
 });
 
-//* Start server
+// Start server
 app.listen(PORT, console.log(`✅ Server running on port ${PORT}`));
