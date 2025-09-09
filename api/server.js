592d5feafd2177f97393d2e011ed36d328664395
jesselessa <jessica.elessa@gmail.com>
2025-09-09 02:49:23 +0200

    tested API route

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3006;
 
-// Middlewares
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//* Middlewares
 app.use(express.json());
 app.use(
   cors({
@@ -13,7 +18,23 @@ app.use(
   })
 );
 
-// Handle errors globally
+// Test route
+app.get("/test", (req, res) => {
+  res
+    .json({
+      message: "test",
+      status: "success",
+    })
+    .status(200);
+});
+
+//* Serve static files in production
+// app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+// });
+
+//* Handle errors globally
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res
@@ -21,5 +42,5 @@ app.use((err, req, res, next) => {
     .json({ error: err.message || "Internal Server Error" });
 });
 
-// Start server
+//* Start server
 app.listen(PORT, console.log(`✅ Server running on port ${PORT}`));
