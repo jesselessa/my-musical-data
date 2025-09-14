1d55044fdb5207d022fac102840c7f4fe08b1bee
jesselessa <jessica.elessa@gmail.com>
2025-09-14 04:37:18 +0200

    updated loader code

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,7 +4,7 @@ export const Loader = () => {
   return (
     <div aria-label="Loading..." role="status">
       <svg
-        class="h-12 w-12 animate-spin stroke-[#b9b9b9]"
+        className="h-12 w-12 animate-spin stroke-[#b9b9b9]"
         viewBox="0 0 256 256"
       >
         <line
@@ -12,72 +12,72 @@ export const Loader = () => {
           y1="32"
           x2="128"
           y2="64"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="195.9"
           y1="60.1"
           x2="173.3"
           y2="82.7"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="224"
           y1="128"
           x2="192"
           y2="128"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="195.9"
           y1="195.9"
           x2="173.3"
           y2="173.3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="128"
           y1="224"
           x2="128"
           y2="192"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="60.1"
           y1="195.9"
           x2="82.7"
           y2="173.3"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="32"
           y1="128"
           x2="64"
           y2="128"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
         <line
           x1="60.1"
           y1="60.1"
           x2="82.7"
           y2="82.7"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="24"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="24"
         ></line>
       </svg>
     </div>
